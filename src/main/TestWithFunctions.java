f13fe671168b4f83881fded64cc144f25e57fc45
pawan <pawan220787@gmail.com>
2024-09-16 13:32:17 +0530

    Renamed function test case.

--- src/main/TestWithFunctions.java
+++ src/main/TestWithFunctions.java
@@ -1,20 +1,14 @@
 package main;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
-
-import java.time.Duration;
 
 import static ImpFiles.Functions.applyFilter;
 import static ImpFiles.Functions.searchProductInSearchField;
 
-public class testingfun {
+public class TestWithFunctions {
     public static void main(String[] args) {
-
-
         String search_product = "mi mobile";
-
         System.setProperty("webdriver.edge.driver", "C:\\Windows\\System32\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.get("https://amazon.in");
